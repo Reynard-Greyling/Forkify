89b123ec99ea84df9babba975cf1ac822d8f1a4f
Reyner <reynard@fintegratetech.co.za>
2024-08-21 11:31:07 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7b0f52b5-3e3f-4000-9e5f-a3b5b73916c8';
+export const MODAL_CLOSE_SEC = 2.5;
